11c7df44feae981ce8d14a53983969ab07c0c3a4
Gabriel <52301750+GabrielOliveira666@users.noreply.github.com>
2024-09-30 14:36:13 -0300

    Update Instalador.bat

--- Instalador.bat
+++ Instalador.bat
@@ -5,20 +5,20 @@ set "CURRENT_VAR=%testes%"
 setlocal EnableDelayedExpansion
 
 rem seta a nova variavel, a variavel atual e a concatenada
-ECHO Insira a localizaÃ§Ã£o da pasta raiz do Firebird
+ECHO Insira a localizacao da pasta raiz do Firebird
 set /p "FB_LOC="
 
 cd %FB_LOC%
 
-rem Valida se a pasta estÃ¡ correta
+rem Valida se a pasta esta correta
 dir | findstr /I "instsvc" >nul
 IF %ERRORLEVEL%==0 (
 	set "CONCATENED_VAR=!CURRENT_VAR!;!FB_LOC!"
 	
-	rem verifica se o conteÃºdo da variavel nova jÃ¡ existe na atual
+	rem verifica se o conteudo da variavel nova ja existe na atual
 	echo !CURRENT_VAR! | findstr /C"!FB_LOC!" >nul
 	
-	rem adiciona a informaÃ§Ã£o a variavel de ambiente se nÃ£o existir, se existir ele retorna erro
+	rem adiciona a informacao a variavel de ambiente se nao existir, se existir ele retorna erro
 	if !ERRORLEVEL!==0 (
 		echo Variavel ja definida "!CURRENT_VAR!"
 		pause
@@ -26,31 +26,26 @@ IF %ERRORLEVEL%==0 (
 		setx testes "!CONCATENED_VAR!" 
 	)
 )ELSE (
-	echo Diretorio incorreto
+	echo Diretorio do Firebird incorreto
 	pause
 )
 
-echo %CONCATENED_VAR%
-
-pause
-
-
 rem seta a nova variavel, a variavel atual e a concatenada
-set "NEW_VAR=C:\Users\G\Desktop\testing"
+set "NEW_VAR=C:\testes"
 rem set "CURRENT_VAR=%testes%"
-rem arrumar a concatenação dessa variável, pois a expansão está fodendo com ela
-set CONCATENED_VAR=%CONCATENED_VAR%;%NEW_VAR%
-echo %CONCATENED_VAR% 
+rem arrumar a concatenaÃ§Ã£o dessa variÃ¡vel, pois a expansÃ£o estÃ¡ fodendo com ela
+set NEW_CONCATENED_VAR=%CONCATENED_VAR%;%NEW_VAR%
+echo %NEW_CONCATENED_VAR% 
 pause
 
-rem verifica se o conteÃºdo da variavel nova jÃ¡ existe na atual
+rem verifica se o conteÃƒÂºdo da variavel nova jÃƒÂ¡ existe na atual
 echo %CURRENT_VAR% | findstr /C"%NEW_VAR%" >nul
 
-rem adiciona a informaÃ§Ã£o a variavel de ambiente se nÃ£o existir, se existir ele retorna erro
+rem adiciona a informaÃƒÂ§ÃƒÂ£o a variavel de ambiente se nÃƒÂ£o existir, se existir ele retorna erro
 if %ERRORLEVEL%==0 (
 	echo Variavel ja definida
 ) else (
-	setx testes "%CONCATENED_VAR%"
+	setx testes "%NEW_CONCATENED_VAR%"
 )
 echo %CURRENT_VAR%
 

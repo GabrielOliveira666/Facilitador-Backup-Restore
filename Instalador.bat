cae05fa904904396d0a46d8cb639afa31cf78fb6
Gabriel <52301750+GabrielOliveira666@users.noreply.github.com>
2024-10-05 16:38:11 -0300

    Update Instalador.bat

--- Instalador.bat
+++ Instalador.bat
@@ -13,17 +13,22 @@ cd %FB_LOC%
 rem Valida se a pasta esta correta
 dir | findstr /I "instsvc" >nul
 IF %ERRORLEVEL%==0 (
+	rem Variavel concatenada está sendo pega na última condição, pois se colocar a pasta correta, ela sempre vai ser definida
+	rem PERIGO: SE PASTA DO FIREBIRD ESTIVER INCORRETA, VAI SUBSTITUIR TUDO PELA DE TESTES PORQUE EU SOU BURRO
 	set "CONCATENED_VAR=!CURRENT_VAR!;!FB_LOC!"
 	
 	rem verifica se o conteudo da variavel nova ja existe na atual
+
 	echo !CURRENT_VAR! | findstr /C"!FB_LOC!" >nul
 
 	rem adiciona a informacao a variavel de ambiente se nao existir, se existir ele retorna erro
+
 	if !ERRORLEVEL!==0 (
-		echo Variavel ja definida
+		echo Variavel ja definida!
 		pause
-	) else (
-		setx PATH "!CONCATENED_VAR!" 
+	) else ( 
+		echo Variavel definida com sucesso!
+		setx /m PATH !CONCATENED_VAR!
 		cls
 	)
 )ELSE (
@@ -33,10 +38,15 @@ IF %ERRORLEVEL%==0 (
 )
 
 rem seta a nova variavel, a variavel atual e a concatenada
+
+cd /
+mkdir testes
+
 set "NEW_VAR=C:\testes"
 set NEW_CONCATENED_VAR=%CONCATENED_VAR%;%NEW_VAR%
 
 rem verifica se o conteudo da variavel nova ja existe na atual
+
 echo %CURRENT_VAR% | findstr /C"%NEW_VAR%" >nul
 
 rem adiciona a informacao a variavel de ambiente se nao existir, se existir ele retorna erro
@@ -44,13 +54,13 @@ if %ERRORLEVEL%==0 (
 	cls
 	echo Variavel ja definida
 	pause
+	exit /b
 ) else (
-	setx PATH "%NEW_CONCATENED_VAR%"
 	cls
-	echo Variavel adicionada
+   setx /m PATH "!NEW_CONCATENED_VAR!"
+	echo Variavel definida com sucesso!
 	pause
 	cls
 )
 endlocal
 exit /b
-
